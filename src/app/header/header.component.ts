025d378c19d84e0b8dd8e0d65f8618b0c9944f74
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-04-06 00:20:42 -0400

    hide sign in button when on auth form

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  displayIfOnAuthPage: boolean;
+  currentRoute: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.currentRoute = route.snapshot.url[0].path;
+  }
 
   ngOnInit(): void {
+    console.log(this.currentRoute);
+    this.displayIfOnAuthPage = false;
+    if (this.currentRoute === 'sign-in' || this.currentRoute === 'sign-up') {
+      this.displayIfOnAuthPage = true;
+    }
   }
 
 }
