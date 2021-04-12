bbc34ac4be90eece41b862dd72439d54b98780a7
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-04-12 03:30:03 -0400

    fixed header route CSS UX to allow current route CSS to stayon 'your cart' button when user is in either cart or checkout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,15 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 export class HeaderComponent implements OnInit {
   onAuthPage: boolean;
   currentRoute: string;
+  shoppingCartOrCheckout = false;
 
   constructor(private route: ActivatedRoute) {
     this.currentRoute = route.snapshot.url[0].path;
   }
+
+  isActive() {
+
+  }
+
 
   ngOnInit(): void {
     this.onAuthPage = false;
     if (this.currentRoute === 'sign-in' || this.currentRoute === 'sign-up') {
       this.onAuthPage = true;
     }
+
+    if (this.currentRoute === 'cart' || this.currentRoute === 'checkout') {
+      this.shoppingCartOrCheckout = true;
+    }
+
+
   }
 }
