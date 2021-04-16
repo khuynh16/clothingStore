7b88d998669bc9a7c4b5a57ffbfccdef688f55dd
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-04-15 22:02:11 -0400

    added quantity button to cards

--- src/app/components/display-clothing-template/display-clothing-template.component.ts
+++ src/app/components/display-clothing-template/display-clothing-template.component.ts
@@ -11,6 +11,8 @@ import { CLOTHING_DATABASE } from '../../mock-clothing';
 })
 export class DisplayClothingTemplateComponent implements OnInit {
   clothingDB = CLOTHING_DATABASE;
+  quantities: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   currentRoute;
 
   constructor(private route: ActivatedRoute, public cartService: CartService) {
