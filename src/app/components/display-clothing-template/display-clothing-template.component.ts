d69abbf0d975e5da53dfa31a249440816bd4ef0e
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-04-16 01:30:57 -0400

    removed 0 as an quantity option

--- src/app/components/display-clothing-template/display-clothing-template.component.ts
+++ src/app/components/display-clothing-template/display-clothing-template.component.ts
@@ -11,7 +11,7 @@ import { CLOTHING_DATABASE } from '../../mock-clothing';
 })
 export class DisplayClothingTemplateComponent implements OnInit {
   clothingDB = CLOTHING_DATABASE;
-  quantities: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  quantities: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   currentRoute;
 
